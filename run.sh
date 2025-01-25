ebcadbd86289c598a78e8487275c064aacf16cfb
Paul Hammant <paul@hammant.org>
2025-01-25 17:04:56 +0000

    wip

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the ReflectiveHttpClientCaller with a restrictive security policy
-java -cp core/target/core-1.0-SNAPSHOT.jar com.example.ReflectiveHttpClientCaller
+./jdk-25/bin/java -cp core/target/core-1.0-SNAPSHOT.jar com.example.ReflectiveHttpClientCaller
--- run_with_policy.sh
+++ run_with_policy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the ReflectiveHttpClientCaller with a grant for a security policy - see Java source
-java -Djava.security.manager -Djava.security.policy=security.policy -cp core/target/core-1.0-SNAPSHOT.jar com.example.ReflectiveHttpClientCaller "$1"
+./jdk-25/bin/java -Djava.security.manager -Djava.security.policy=security.policy -cp core/target/core-1.0-SNAPSHOT.jar com.example.ReflectiveHttpClientCaller "$1"
