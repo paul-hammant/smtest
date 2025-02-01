9ab26be3725c2221635027c7d96ccc1b21baef4d
Paul Hammant <paul@hammant.org>
2025-02-01 17:13:36 +0000

    we don't want this

--- run_with_specific_policy.sh
+++ run_with_specific_policy.sh
@@ -4,7 +4,7 @@
 
 JAVA_HOME=~/.jdks/corretto-21.0.6
 ~/.jdks/corretto-21.0.6/bin/java --version
-~/.jdks/corretto-21.0.6/bin/java -Djava.security.manager -Djava.security.policy=specific_security.policy -cp core/target/core-1.0-SNAPSHOT.jar com.example.ReflectiveHttpClientCaller "$1"
+~/.jdks/corretto-21.0.6/bin/java -Djava.security.debug=access -Djava.security.manager -Djava.security.policy=specific_security.policy -cp core/target/core-1.0-SNAPSHOT.jar com.example.ReflectiveHttpClientCaller "$1"
 #JAVA_HOME=/home/paul/jdk-25
 #./jdk-25/bin/java --version
 #./jdk-25/bin/java -Djava.security.manager -Djava.security.policy=specific_security.policy -cp core/target/core-1.0-SNAPSHOT.jar com.example.ReflectiveHttpClientCaller "$1"
--- test/src/main/java/com/example/TestHttpClient.java
+++ test/src/main/java/com/example/TestHttpClient.java
@@ -3,36 +3,95 @@ package com.example;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.security.AccessControlException;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.security.ProtectionDomain;
+
+import static java.security.AccessController.doPrivileged;
 
 public class TestHttpClient {
 
+    static class FooClassLoader extends URLClassLoader{
+
+        public FooClassLoader(URL[] urls, ClassLoader parent) {
+            super(urls, parent);
+        }
+    }
+
+    // Called using reflection from ReflectiveHttpClientCaller
     public String fetchAndValidateResponse() throws IOException, InterruptedException, URISyntaxException {
-            HttpClient client = HttpClient.newHttpClient();
+
+        // Historical vulns with Java sandbox/ permissions
+        try {
+            ProtectionDomain foo = this.getClass().getProtectionDomain();
+            throw new AssertionError("nope");
+        } catch (AccessControlException e) {
+            // expected
+        }
+
+        URLClassLoader cl = ((URLClassLoader) this.getClass().getClassLoader());
+
+        try {
+            new FooClassLoader(new URL[0], this.getClass().getClassLoader());
+            throw new AssertionError("nope");
+        } catch (AccessControlException e) {
+            // expected
+        }
+
+        // Add more from https://github.com/pwntester/ysoserial here....
+
+
+        HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(new URI("https://httpbin.org/get"))
                     .header("Accept", "application/json")
                     .build();
-
-        HttpResponse<String> response = null;
-
         try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String body = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+            //System.out.println("body: " + body);
+            return body.contains("{\n" +
+                    "  \"args\": {}, \n" +
+                    "  \"headers\": {\n" +
+                    "    \"Accept\": \"application/json\", \n" +
+                    "    \"Host\": \"httpbin.org\", \n" +
+                    "    \"User-Agent\": \"Java-http-client") ? "JSON as expected": "Not the expected JSON : " + body;
         } catch (SecurityException e) {
             return "Security Exception, msg: " + e.getMessage();
         }
 
-        String body = response.body();
-        //System.out.println("body: " + body);
-        return body.contains("{\n" +
-                "  \"args\": {}, \n" +
-                "  \"headers\": {\n" +
-                "    \"Accept\": \"application/json\", \n" +
-                "    \"Host\": \"httpbin.org\", \n" +
-                "    \"User-Agent\": \"Java-http-client") ? "JSON as expected": "Not the expected JSON : " + body;
+
     }
+//    public String fetchAndValidateResponse() throws IOException, InterruptedException, URISyntaxException {
+//            HttpClient client = HttpClient.newHttpClient();
+//            HttpRequest request = HttpRequest.newBuilder()
+//                    .uri(new URI("https://httpbin.org/get"))
+//                    .header("Accept", "application/json")
+//                    .build();
+//        return AccessController.doPrivileged(new PrivilegedAction<String>() {
+//            @Override
+//            public String run() {
+//                try {
+//                    String body = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+//                    //System.out.println("body: " + body);
+//                    return body.contains("{\n" +
+//                            "  \"args\": {}, \n" +
+//                            "  \"headers\": {\n" +
+//                            "    \"Accept\": \"application/json\", \n" +
+//                            "    \"Host\": \"httpbin.org\", \n" +
+//                            "    \"User-Agent\": \"Java-http-client") ? "JSON as expected": "Not the expected JSON : " + body;
+//                } catch (SecurityException | IOException | InterruptedException e) {
+//                    return "Security Exception, msg: " + e.getMessage();
+//                }
+//            }
+//        });
+//
+//    }
+
+
 }
